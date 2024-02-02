6b29a64701ed4ded1e7cdcb0bc25aecc4a08fe2a
avneetkaur77 <121680092+avneetkaur77@users.noreply.github.com>
2024-02-02 19:38:49 +0530

    ok

---
+++ app/src/main/java/com/example/ongoing/MainActivity.kt
@@ -0,0 +1,122 @@
+package com.example.ongoing
+
+import android.os.Bundle
+import android.text.Layout.Alignment
+import android.widget.EditText
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.AccountCircle
+import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.Divider
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.ongoing.ui.theme.OngoingTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            OngoingTheme {
+
+
+                Text(text ="Ongoing Camps",
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 25.sp,
+                    fontStyle = FontStyle.Italic)
+                Icon(Icons.Rounded.AccountCircle,
+                    contentDescription = null,
+                    modifier = Modifier
+                        .offset(x = 370.dp, y = 45.dp)
+                        .clickable { })
+                Box(contentAlignment = androidx.compose.ui.Alignment.TopCenter) {
+                    Image(
+                        painter = painterResource(id = R.drawable.abc),
+                        contentDescription = null,
+                        contentScale = ContentScale.Fit,
+
+                        modifier = Modifier
+                            .size(420.dp)
+                            .padding(10.dp)
+                    )
+
+
+                }
+                Text(text ="Title",
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 25.sp,
+                    fontStyle = FontStyle.Italic,
+                    modifier = Modifier
+                        .offset(x=10.dp,y=340.dp))
+
+                Text(text ="Discription",
+                    fontWeight = FontWeight.SemiBold,
+                    fontSize = 20.sp,
+                    fontStyle = FontStyle.Italic,
+                    modifier = Modifier
+                        .offset(x=10.dp,y=370.dp))
+                Divider(color = Color.Black, modifier = Modifier
+                    .fillMaxWidth()
+                    .width(1.dp)
+                    .offset(y = 400.dp))
+
+
+                Image(
+                    painter = painterResource(id = R.drawable.abc),
+                    contentDescription = null,
+                    contentScale = ContentScale.Fit,
+
+                    modifier = Modifier
+                        .size(420.dp)
+                        .padding(10.dp)
+                )
+
+
+
+
+
+
+
+
+
+
+
+            }
+        }
+    }
+
+
+}
